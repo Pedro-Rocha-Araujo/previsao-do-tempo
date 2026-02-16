bba70761b1f972293653431139cc5c8c24b46cde
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-02-16 11:57:58 -0300

    Api se comunicando e layout estruturado

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react"
 import Header from "./Header.js"
+import Footer from "./Footer.js"
 import './App.css';
 
 function App() {
@@ -24,35 +25,41 @@ function App() {
     <div className="main">
       <Header />
       <div className="card">
-            <form onSubmit={enviarFormulariuo} method="post">
-                <i className="fa-solid fa-location-dot"></i>
-                <input onChange={capturarInput} type="text" value={cidade} />
-                <button type="submit">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </form>
-            {informacoes ? <div className="infos">
-              <div className="info-principal">
-                <h2>{informacoes.name}</h2>
-                <div className="campo-imagem"></div>
+        <form onSubmit={enviarFormulariuo} method="post">
+            <input onChange={capturarInput} type="text" value={cidade} />
+            <button type="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </form>
+        {informacoes ? <div className="infos">
+          <div className="info-principal">
+            <div>
+              <h2>{informacoes.name}</h2>
+              <p>{informacoes.main.temp}°</p>
+            </div>
+            <div className="campo-imagem">
+              <img src={`http://openweathermap.org/img/wn/${informacoes.weather[0].icon}.png`} />
+            </div>
+          </div>
+          <div className="infos-secundarias">
+            <div className="info-secundaria">
+              <h3>Max-Temp</h3>
+              <div>
+                <i className="fa-solid fa-temperature-full"></i>
+                <p>{informacoes.main.temp_min}°</p>
               </div>
-              <div className="infos-secundarias">
-                <div className="info-secundaria">
-                  <p>{informacoes.main.temp}</p>
-                </div>
-                <div className="info-secundaria">
-                  <h3>Max-Temp</h3>
-                  <i className="fa-solid fa-temperature-full"></i>
-                  <p>{informacoes.main.temp_min}</p>
-                </div>
-                <div className="info-secundaria">
-                  <h3>Min-Temp</h3>
-                  <i className="fa-solid fa-temperature-full"></i>
-                  <p>{informacoes.main.temp_max}</p>
-                </div>
+            </div>
+            <div className="info-secundaria">
+              <h3>Min-Temp</h3>
+              <div>
+                <i className="fa-solid fa-temperature-full"></i>
+                <p>{informacoes.main.temp_max}°</p>
               </div>
-            </div> : null}
-        </div>  
+            </div>
+          </div>
+        </div> : null}
+      </div>  
+      <Footer />
     </div>
   );
 }
